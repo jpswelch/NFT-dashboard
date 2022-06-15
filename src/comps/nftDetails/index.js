37b8d4e7db330727e992d335fa3a9620014d60c0
jordang7 <77jgreenberg77@gmail.com>
2022-06-15 11:27:44 -0400

    Add owner to display

--- src/comps/nftDetails/index.js
+++ src/comps/nftDetails/index.js
@@ -1,36 +1,45 @@
-import React, {useEffect, useState} from 'react'
-import Alert from '../../assets/alert.svg'
-import './style.css'
+import React, { useEffect, useState } from "react";
+import Alert from "../../assets/alert.svg";
+import "./style.css";
 
-const NftDetails = ({data, color}) => {
+const NftDetails = ({ data, color }) => {
   return (
     <div className="nft-cont">
       <div className="img-container">
-        <img onError={(event) => {
-                      event.target.classList.add("error-image")
-                      event.target.classList.remove("nft-img")
-                      }} className="nft-img" src={data ?.external_data?.image}></img>
+        <img
+          onError={(event) => {
+            event.target.classList.add("error-image");
+            event.target.classList.remove("nft-img");
+          }}
+          className="nft-img"
+          src={data?.external_data?.image}
+        ></img>
       </div>
-      <div className="nft-details"  style={{backgroundColor:color}}>
-        <h1>{data ?.external_data?.name}</h1>
-        <h2>Token ID : {data ?.token_id}</h2>
-        <p>{data ?.external_data?.description}</p>
-        <table className="nft-table"> 
-          {data ?.external_data?.attributes ? 
-          <>
-          {data.external_data.attributes.map((o,i)=>{
-              return <tr key={i}>
-                        <td> {o.trait_type} </td>
-                        <td> {o.value} </td>
-                    </tr>
-          })}
-          </>
-          : null}
+      <div className="nft-details" style={{ backgroundColor: color }}>
+        <h1>{data?.external_data?.name}</h1>
+        <h2>Token ID : {data?.token_id}</h2>
+        <h3>
+          Owner: {data?.owner_address.substring(0, 4)}...
+          {data?.owner_address.substring(37, 41)}
+        </h3>
+        <p>{data?.external_data?.description}</p>
+        <table className="nft-table">
+          {data?.external_data?.attributes ? (
+            <>
+              {data.external_data.attributes.map((o, i) => {
+                return (
+                  <tr key={i}>
+                    <td> {o.trait_type} </td>
+                    <td> {o.value} </td>
+                  </tr>
+                );
+              })}
+            </>
+          ) : null}
         </table>
       </div>
     </div>
-  )
-}
+  );
+};
 
-
-export default NftDetails
+export default NftDetails;
