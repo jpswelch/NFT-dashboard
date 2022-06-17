163623bd79868e14dc1cd7e970b93838d0cd364c
jordang7 <77jgreenberg77@gmail.com>
2022-06-17 16:14:26 -0400

    beginnings of minting gift nft using ipfs and ethers

--- src/comps/giftModal/index.js
+++ src/comps/giftModal/index.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
   Modal,
   ModalOverlay,
@@ -10,8 +11,27 @@ import {
   Button,
 } from "@chakra-ui/react";
 
+import Form from "react-bootstrap/Form";
+import Col from "react-bootstrap/Col";
+
+// import { mintGiftNFT } from "../../actions/nftActions";
+
 export default function GiftModal({ CollectionName, ownerAddressArray }) {
   const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const [name, setName] = useState();
+  const [description, setDescriptionData] = useState();
+  const [image, setImageData] = useState();
+
+  const handleMint = async (e) => {
+    e.preventDefault();
+    //   let success = await mintGiftNFT(name, description, image, recipient);
+    // if (success) {
+    //   alert("Minting is complete!");
+    // } else {
+    //   alert("Minting failed!");
+    // }
+  };
   return (
     <>
       <Button onClick={onOpen} colorScheme="blue">
@@ -29,14 +49,23 @@ export default function GiftModal({ CollectionName, ownerAddressArray }) {
           </ModalHeader>
           <ModalCloseButton />
           <ModalBody color={"whiteAlpha.900"}>
-            NFT upload and send + share on twitter
+            <Form.Group controlId="formFile">
+              <Form.Control
+                type="file"
+                onChange={(e) => setImageData(e.target.files[0])}
+                className="mb-3"
+                required
+              />
+            </Form.Group>
           </ModalBody>
 
           <ModalFooter>
             <Button colorScheme="blue" mr={3} onClick={onClose}>
               Close
             </Button>
-            <Button colorScheme="green">Submit</Button>
+            <Button colorScheme="green" onClick={handleMint}>
+              Mint
+            </Button>
           </ModalFooter>
         </ModalContent>
       </Modal>
