22720a25003d4194ae735ed667ca8017389ac226
Jean-Pierre Welch <jpswelch@gmail.com>
2022-06-19 09:50:26 -0400

    adding unstoppable domian auth with moralis

---
+++ src/connectors.js
@@ -0,0 +1,28 @@
+// Instanciate your other connectors.
+import {UAuthMoralisConnector} from '@uauth/moralis'
+
+export const injected = {}
+
+// export const walletconnect = {provider: 'walletconnect'}
+
+UAuthMoralisConnector.setUAuthOptions({
+  clientID: process.env.REACT_APP_CLIENT_ID,
+  redirectUri: process.env.REACT_APP_REDIRECT_URI,
+
+  // Scope must include openid and wallet
+  scope: 'openid wallet',
+
+  // Injected and walletconnect connectors are required
+  connectors: {injected},
+})
+
+const uauth = {connector: UAuthMoralisConnector}
+
+// const connectors: Record<string, any> = {
+const connectors = {  
+  injected,
+  // walletconnect,
+  uauth,
+}
+
+export default connectors
--- src/index.js
+++ src/index.js
@@ -4,11 +4,18 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
+import { MoralisProvider } from 'react-moralis';
+
 ReactDOM.render(
   <React.StrictMode>
-    <ChakraProvider>
+    <MoralisProvider
+      serverUrl={process.env.REACT_APP_MORALIS_SERVER}
+      appId={process.env.REACT_APP_MORALIS_APP_ID}
+    >
+      <ChakraProvider>
       <App />
     </ChakraProvider>
+    </MoralisProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
